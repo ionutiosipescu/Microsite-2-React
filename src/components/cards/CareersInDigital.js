9b9f92ebeb19cc84337ab661d4009369f6d11b02
unknown <anton.ostopovici@tarainteractive.com>
2022-07-13 17:16:38 +0300

    Overview Subpage 80%

--- src/components/cards/CareersInDigital.js
+++ src/components/cards/CareersInDigital.js
@@ -1,43 +1,44 @@
-import React from 'react'
-import styled from 'styled-components'
-import YellowButton from '../buttons/YellowButton'
+import React from "react";
+import styled from "styled-components";
+import YellowButton from "../buttons/YellowButton";
 
 const Card = styled.div`
-	background: var(--gray3);
-	display: flex;
-	flex-direction: column;
-	justify-content: space-around;
-	// align-items: center;
-	height: 400px;
-	padding: 30px;
+  background: var(--gray3);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  // align-items: center;
+  height: 400px;
+  padding: 30px;
 
-	h1 {
-		font-weight: bold;
-		text-transform: uppercase;
-	}
+  h2 {
+    font-weight: bold;
+    text-transform: uppercase;
+  }
 
-	p {
-		font-size: 1.25rem;
-		// color: gray;
-	}
+  p {
+    font-size: 1.25rem;
+    // color: gray;
+  }
 
-	button {
-		// color: red;
-		justify-self: start;
-	}
-`
+  button {
+    // color: red;
+    justify-self: start;
+    font-size: 14px;
+  }
+`;
 
 const CareersInDigital = () => {
-	return (
-		<Card>
-			<h1>Careers in digital</h1>
-			<p>
-				Mollit non voluptate ipsum cillum. Cillum mollit deserunt nulla sunt et
-				enim voluptate. Labore reprehenderit et in aliquip laborum pariatur.
-			</p>
-			<YellowButton text="SEARCH AND APPLY" width="auto" />
-		</Card>
-	)
-}
+  return (
+    <Card>
+      <h2>Careers in digital</h2>
+      <p>
+        Mollit non voluptate ipsum cillum. Cillum mollit deserunt nulla sunt et
+        enim voluptate. Labore reprehenderit et in aliquip laborum pariatur.
+      </p>
+      <YellowButton text="SEARCH AND APPLY" width="50%" />
+    </Card>
+  );
+};
 
-export default CareersInDigital
+export default CareersInDigital;
--- src/pages/about/subPages/Overview.js
+++ src/pages/about/subPages/Overview.js
@@ -1,107 +1,88 @@
-import React from 'react'
-import { Button, Card, Row, Col, Container } from 'react-bootstrap'
-import YellowButton from '../../../components/buttons/YellowButton'
-import { NewsCell } from './subPages.style'
-import CardOverview from '../../../components/cards/CardOverview'
-import CareersInDigital from '../../../components/cards/CareersInDigital'
-import image from '../../../components/images/ConnorWine'
+import React from "react";
+import { Button, Card, Row, Col, Container } from "react-bootstrap";
+import YellowButton from "../../../components/buttons/YellowButton";
+import { NewsCell } from "./subPages.style";
+import CardOverview from "../../../components/cards/CardOverview";
+import CareersInDigital from "../../../components/cards/CareersInDigital";
+import image from "../../../components/images/ConnorWine";
 
 const Overview = () => {
-	// Card overview content from backend
-	const cardContent = {
-		imageSrc: image,
-		name: 'Connor Colquhoun',
-		position: 'wine connoisseur',
-		country: 'japan',
-	}
+  // Card overview content from backend
+  const cardContent = {
+    imageSrc: image,
+    name: "Connor Colquhoun",
+    position: "wine connoisseur",
+    country: "japan",
+  };
 
-	return (
-		<>
-			<Row className="">
-				<Col className="p-5">
-					<h1 className="my-4">
-						Lorem IpsuCulpa sint magna ullamco irure reprehenderit occaecat
-					</h1>
-					<p>
-						Esse incididunt deserunt esse aute. Est proident excepteur deserunt
-						cillum labore ea esse eiusmod do aliqua anim. Eiusmod elit enim id
-						dolor aliquip occaecat velit proident.
-					</p>
-					<p>
-						Esse incididunt deserunt esse aute. Est proident excepteur deserunt
-						in in minim sunt exercitation et ipsum dolor et. Commodo cillum
-						dolor aliquip occaecat velit proident.
-					</p>
-					<Container
-						className=" my-4 p-4 "
-						style={{ background: 'var(--gray1' }}
-					>
-						<Row>
-							<Col>
-								<h4 className="fw-bold text-primary">Our Capabilities</h4>
-								<Row className="my-4 fw-bold">
-									<Col>
-										<div>Digital Strategy</div>
-										<div>Digital Strategy</div>
-									</Col>
-									<Col>
-										<div>Digital Strategy</div>
-										<div>Digital Strategy</div>
-									</Col>
-									<Col>
-										<div>Digital Strategy</div>
-										<div>Digital Strategy</div>
-										{/* <img src={digital}></img> */}
-									</Col>
-								</Row>
-								<YellowButton
-									text={'Read more about our Capabilities'}
-								></YellowButton>
-							</Col>
-						</Row>
-					</Container>
+  return (
+    <>
+      <Row className="">
+        <Col className="p-5">
+          <h1 className="my-4">
+            Lorem IpsuCulpa sint magna ullamco irure reprehenderit occaecat
+          </h1>
+          <p>
+            Esse incididunt deserunt esse aute. Est proident excepteur deserunt
+            cillum labore ea esse eiusmod do aliqua anim. Eiusmod elit enim id
+            dolor aliquip occaecat velit proident.
+          </p>
+          <p>
+            Esse incididunt deserunt esse aute. Est proident excepteur deserunt
+            in in minim sunt exercitation et ipsum dolor et. Commodo cillum
+            dolor aliquip occaecat velit proident.
+          </p>
+          <div className="my-3 border-bottom border-top border-3">
+            <h4 className="text-info py-2">
+              {" "}
+              Brazilian Restructuring and Insolvency Engagements
+            </h4>
+            <p>
+              Esse incididunt deserunt esse aute. Est proident excepteur
+              deserunt in in minim sunt exercitation et ipsum dolor et. Commodo
+              cillum dolor aliquip occaecat velit proident.
+            </p>
+          </div>
+          <Container
+            className=" my-4 p-4 "
+            style={{ background: "var(--gray1" }}
+          >
+            <Row className="rounded">
+              <Col>
+                <h4 className="fw-bold text-info">Our Services</h4>
+                <Row className="my-4 fw-bold">
+                  <Col>
+                    <div>Digital Strategy Digital Strategy</div>
+                    <div>Digital Strategy Digital Strategy</div>
+                    <div>Digital Strategy Digital Strategy</div>
+                    <div>Digital Strategy Digital Strategy </div>
+                    <div>Digital Strategy Digital Strategy</div>
+                    <div>Digital Strategy Digital Strategy</div>
+                  </Col>
+                  <Col>
+                    <div>Digital Strategy Digital Strategy</div>
+                    <div>Digital Strategy Digital Strategy</div>
+                    <div>Digital Strategy Digital Strategy</div>
+                    <YellowButton
+                      text={"Read more about our Services"}
+                      width="80%"
+                      className="mt-4 fs-7"
+                    />
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+        <Col className="col-4 p-0">
+          <CardOverview {...cardContent} />
+          <CardOverview {...cardContent} />
 
-					<h2 className="fw-bold">Related Insight</h2>
-					<Row>
-						<Col>
-							<NewsCell>
-								<h4 className="fw-bold">Lorem</h4>
-								<p className="text-muted">15/5/2020</p>
-							</NewsCell>
-							<NewsCell>
-								<h4 className="fw-bold">Lorem</h4>
-								<p className="text-muted">15/5/2020</p>
-							</NewsCell>
-							<NewsCell>
-								<h4 className="fw-bold">Lorem</h4>
-								<p className="text-muted">15/5/2020</p>
-							</NewsCell>
-						</Col>
-						<Col>
-							<NewsCell>
-								<h4 className="fw-bold">Lorem</h4>
-								<p className="text-muted">15/5/2020</p>
-							</NewsCell>
-							<NewsCell>
-								<h4 className="fw-bold">Lorem</h4>
-								<p className="text-muted">15/5/2020</p>
-							</NewsCell>
-							<NewsCell>
-								<h4 className="fw-bold">Lorem</h4>
-								<p className="text-muted">15/5/2020</p>
-							</NewsCell>
-						</Col>
-					</Row>
-				</Col>
-				<Col className="col-4 p-0">
-					<CardOverview {...cardContent} />
-					<CardOverview {...cardContent} />
+          <CareersInDigital />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-					<CareersInDigital />
-				</Col>
-			</Row>
-		</>
-	)
-}
-
-export default Overview
+export default Overview;
